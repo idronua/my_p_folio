9a295faf0088eb92d7c99651a1dac705da1ad18c
Andre <can124ever@gmail.com>
2022-05-17 23:04:41 -0400

    Remove dynamic effects

--- js/javascript.js
+++ js/javascript.js
@@ -2,14 +2,17 @@ $(".menu-toggler").click(function(){
     $(".navbar-nav").toggleClass("active");
 });
 
-AOS.init({
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 1000, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: true, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
-});
+// For dymanic server
+
+// AOS.init({
+//   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//   offset: 120, // offset (in px) from the original trigger point
+//   delay: 1000, // values from 0 to 3000, with step 50ms
+//   duration: 1000, // values from 0 to 3000, with step 50ms
+//   easing: 'ease', // default easing for AOS animations
+//   once: false, // whether animation should happen only once - while scrolling down
+//   mirror: true, // whether elements should animate out while scrolling past them
+//   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+// });
